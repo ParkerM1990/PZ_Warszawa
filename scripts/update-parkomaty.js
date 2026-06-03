191511a006545ad6f2949caa2bc879da39186e5b
ParkerM1990 <madrzak1990@gmail.com>
2026-06-03 11:13:00 +0200

    Update update-parkomaty.js

--- scripts/update-parkomaty.js
+++ scripts/update-parkomaty.js
@@ -36,7 +36,7 @@ function parseCSV(text) {
       location: r[1]?.trim(),
       lng: parseFloat(r[2]),
       lat: parseFloat(r[3]),
-      node: r[4]?.trim()
+      TerminalStructureName: r[5]?.trim()
     }));
 }
 
